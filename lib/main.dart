7205fcb676884fc1826e7dc42ff0feaaca962ab4
Vivekchavda1374 <viveksinhchavda@gmail.com>
2025-12-16 19:04:12 +0530

    feat: Implement Firebase integration and add a splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hetanshi_enterprise/firebase_options.dart';
 import 'package:hetanshi_enterprise/screens/login_screen.dart';
+import 'package:hetanshi_enterprise/screens/splash_screen.dart';
 import 'package:hetanshi_enterprise/screens/dashboard_screen.dart';
 import 'package:hetanshi_enterprise/screens/product/product_list_screen.dart';
 import 'package:hetanshi_enterprise/screens/party/party_list_screen.dart';
@@ -8,7 +11,11 @@ import 'package:hetanshi_enterprise/screens/user/user_list_screen.dart';
 import 'package:hetanshi_enterprise/screens/order/order_list_screen.dart';
 import 'package:hetanshi_enterprise/screens/order/order_history_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -85,8 +92,9 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      initialRoute: '/login',
+      initialRoute: '/splash',
       routes: {
+        '/splash': (context) => const SplashScreen(),
         '/login': (context) => const LoginScreen(),
         '/dashboard': (context) => const DashboardScreen(),
         '/products': (context) => const ProductListScreen(),
